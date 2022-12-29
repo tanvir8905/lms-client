0789bacc1165b7564a094e8273bcd0504bf5183b
tanvir8905 <tanvir8905.com>
2022-12-29 15:14:22 +0600

    dependency install going on

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to LMS Client</h1>
+      <h1 className='text-primary'>Welcome to LMS Client</h1>
     </div>
   );
 }
